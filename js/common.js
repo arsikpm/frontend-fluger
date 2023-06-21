e91810089efaa81d24031db96927844a3213e00c
Ars Pym <113248995+arsikpm@users.noreply.github.com>
2023-06-21 11:21:41 +0300

    Add responsive versions, new pages, ui panels

--- js/common.js
+++ js/common.js
@@ -15,3 +15,21 @@ $( function() {
   $( "#amount" ).val( "$" + $( "#slider_range" ).slider( "values", 1 ) +
 	" - $" + $( "#slider_range" ).slider( "values", 1 ) );
 } );
+
+// range slider 2
+$(function () {
+	$("#slider_range_2").slider({
+		range: true,
+		min: 0,
+		max: 500,
+		values: [75, 300],
+		slide: function (event, ui) {
+			$("#amount-3").val(ui.values[0])
+		},
+		slide: function (event, ui) {
+			$("#amount-4").val(ui.values[1])
+		}
+	})
+	$("#amount").val("$" + $("#slider_range_2").slider("values", 1) +
+		" - $" + $("#slider_rang_2").slider("values", 1))
+});
