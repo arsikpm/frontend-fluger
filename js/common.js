435703f88d14707c34893aeef289da95775c050d
Ars Pym <113248995+arsikpm@users.noreply.github.com>
2023-06-21 11:19:32 +0300

    Add responsive (index, products, favorites, login, not_found, cart).

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
-function onClickButton() {
-	document.getElementById("dropdown").classList.remove("hidden");
-}
+// range slider
+$( function() {
+  $( "#slider_range" ).slider({
+	range: true,
+	min: 0,
+	max: 500,
+	values: [ 75, 300 ],
+	slide: function( event, ui ) {
+	  $( "#amount-1" ).val(ui.values[ 0 ]);
+	},
+	slide: function( event, ui ) {
+	  $( "#amount-2" ).val(ui.values[ 1 ]);
+	}
+  });
+  $( "#amount" ).val( "$" + $( "#slider_range" ).slider( "values", 1 ) +
+	" - $" + $( "#slider_range" ).slider( "values", 1 ) );
+} );
